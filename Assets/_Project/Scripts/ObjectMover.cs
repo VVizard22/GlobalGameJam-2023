0d78c21b7d2dcdb04ddb61f5f7ab882bb38d88e1
mario <rusconimario@hotmail.com>
2023-02-04 23:11:19 -0300

    Added basica camera movement and setting up level template

---
+++ Assets/_Project/Scripts/ObjectMover.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GGJ
+{
+    public class ObjectMover : MonoBehaviour
+    {
+        [SerializeField] Transform _Target;
+        [SerializeField] float multiplyer = 0.1f;
+        [SerializeField]
+        [Range(0f, 05f)]
+        float _errorMargin = 2f;
+        Vector3 _directionVector = Vector3.zero;
+
+
+        // Update is called once per frame
+        private void FixedUpdate()
+        {
+            _directionVector = _Target.position - transform.position;
+            _directionVector.Normalize();
+            _directionVector *= multiplyer;
+
+            if (Vector3.Distance(transform.position, _Target.position) <= _errorMargin)
+                return;
+
+            _directionVector.z = 0;
+            transform.position += _directionVector;
+        }
+
+    }
+}
